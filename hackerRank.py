d68169ec1de7642ca1ff9fc3e7281926d3560ead
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-07-06 16:32:11 -0300

    01

---
+++ hackerRank.py
@@ -0,0 +1,15 @@
+from collections import deque
+dec = deque()
+sw = {
+    "append": dec.append,
+    "appendleft": dec.appendleft,
+    "pop": dec.pop,
+    "popleft": dec.popleft
+}
+n = int(input())
+for i in range(n):
+    data = map(str,input().split())
+    try:
+        sw[data[0]](int(data[1]))
+    except:
+        sw[data[0]]()
