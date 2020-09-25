a19d65dffe0187b1b68bddbd7d68f5be5935cfa6
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-09-25 12:58:56 -0300

    mySubmit

--- hackerRank.py
+++ hackerRank.py
@@ -1,17 +1,9 @@
-def howMuth(n):
-    tx = ""
-    for i in range(n):
-        tx=tx+"#"
-    return tx+" "
-fil = open("./text")
-li = fil.read().split('\n')
-fil.close()
-n = 4
-tx = howMuth(n)
-for i in range(len(li)):
-    li[i] = tx +li[i]
-tx = "\n".join(li)
-fil = open("out","w")
-fil.write(tx)
-fil.close()
-print(tx)
+Regex_Pattern = r'hackerrank'	# Do not delete 'r'.
+
+import re
+
+Test_String = input()
+
+match = re.findall(Regex_Pattern, Test_String)
+
+print("Number of matches :", len(match))
