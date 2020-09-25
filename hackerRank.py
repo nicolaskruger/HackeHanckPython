f73f1cdf0fceeb324d5f1bc59e692e5eaf8898e2
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-09-25 17:29:01 -0300

    mySubmit

---
+++ hackerRank.py
@@ -0,0 +1,6 @@
+#https://www.hackerrank.com/challenges/matching-word-non-word/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+Regex_Pattern = r"\w{3}\W\w{10}\W\w{3}"	# Do not delete 'r'.
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
